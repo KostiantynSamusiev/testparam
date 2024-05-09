b3cf6ca33260ff89e3c32e58a5af7660d91422f2
Kostiantyn Samusiev <djfoozyhook@gmail.com>
2024-05-09 14:35:30 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // Get the current URL
     var currentUrl = window.location.href
     var urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.size)
     var links = document.getElementsByTagName("a");
 
     // Iterate over each <a> element and update its href attribute
     for (var i = 0; i < links.length; i++) {
         var href = links[i].getAttribute("href");
         // Check if href is not null and if it's not a hash link
-        if (href && href.charAt(0) !== "#") {
+        if (urlParams.size == 0) {
             links[i].setAttribute("href", href);
         } else {
             links[i].setAttribute("href", urlParams.toString());
