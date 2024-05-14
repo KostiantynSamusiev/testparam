7c72b247b44167166f0b4209de7f9c1d341686a8
Kostiantyn Samusiev <djfoozyhook@gmail.com>
2024-05-14 12:27:11 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
     for (var i = 0; i < links.length; i++) {
         var href = links[i].getAttribute("href");
         // Check if href is not null and if it's not a hash link
-        if (urlParams.size == 0) {
-            links[i].setAttribute("href", href);
-        } else {
-            links[i].setAttribute("href", customLink + "?" + urlParams.toString());
+        // if (urlParams.size == 0) {
+        //     links[i].setAttribute("href", href);
+        // } else {
+        //     links[i].setAttribute("href", customLink + "?" + urlParams.toString());
 
-        }
-
+        // }
+        links[i].setAttribute("href", customLink + "?" + urlParams.toString());
     }
 
 });
